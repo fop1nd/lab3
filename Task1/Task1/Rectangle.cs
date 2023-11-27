3c71e19e6b2dbc83319b25553f714e8a816f731b
fop1nd <152083647+fop1nd@users.noreply.github.com>
2023-11-27 20:35:44 +0200

    Add files via upload

---
+++ Task1/Task1/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the length of the first side of the rectangle:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the length of the second side of the rectangle:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Perimeter of a rectangle: {rectangle.Perimeter}");
+        Console.WriteLine($"Area of ​​the rectangle: {rectangle.Area}");
+
+        Console.ReadLine();
+    }
+}
