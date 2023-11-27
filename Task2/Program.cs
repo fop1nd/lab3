3d52fd357c9310abe8cc18b5ea9161fa95a8a6e5
fop1nd <152083647+fop1nd@users.noreply.github.com>
2023-11-27 20:50:34 +0200

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Point A = new Point(0, 0, "A");
+        Point B = new Point(3, 0, "B");
+        Point C = new Point(3, 4, "C");
+
+        Figure figure = new Figure(A, B, C);
+        figure.CalculatePerimeter();
+
+        Console.ReadLine();
+    }
+}
