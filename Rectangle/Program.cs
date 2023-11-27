4d5d7c8f89fbfce4beb63e4c5303a5604dd180cd
fop1nd <152083647+fop1nd@users.noreply.github.com>
2023-11-27 20:36:10 +0200

    Add files via upload

---
+++ Rectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        double inputSide1, inputSide2;
+
+        Console.WriteLine("Enter the length of the first side of the rectangle:");
+        while (!double.TryParse(Console.ReadLine(), out inputSide1) || inputSide1 <= 0)
+        {
+            Console.WriteLine("Enter a valid value for the length of the first side (positive number):");
+        }
+
+        Console.WriteLine("Enter the length of the second side of the rectangle:");
+        while (!double.TryParse(Console.ReadLine(), out inputSide2) || inputSide2 <= 0)
+        {
+            Console.WriteLine("Enter a valid value for the length of the other side (positive number):");
+        }
+
+        Rectangle rectangle = new Rectangle(inputSide1, inputSide2);
+
+        Console.WriteLine($"Perimeter of a rectangle: {rectangle.Perimeter}");
+        Console.WriteLine($"Area of ​​the rectangle: {rectangle.Area}");
+
+        Console.ReadLine();
+    }
+}
